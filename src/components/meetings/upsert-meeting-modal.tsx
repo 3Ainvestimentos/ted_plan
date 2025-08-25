8a6290f6cfc6960bc0fa3e114cd5751bcf88595a
Matheus Mattos <matheus@3ainvestimentos.com.br>
2025-08-25 15:05:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/meetings/upsert-meeting-modal.tsx
+++ src/components/meetings/upsert-meeting-modal.tsx
@@ -3,7 +3,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useForm, useFieldArray } from "react-hook-form";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
